fc80b7b9509c6c5e793e3f4d41e8b46237f1c2a7
VŨ NGUYỄN LONG <20120405@student.hcmus.edu.vn>
2021-04-21 21:44:07 +0700

    User Data

--- CourseRegistrationSystem/LoginSystem.cpp
+++ CourseRegistrationSystem/LoginSystem.cpp
@@ -6,50 +6,78 @@
 
 using namespace std;
 
+const string userData = "data/users.csv";
+
 struct User {
 	int id;
 	string password;
 	bool isAdmin;
-	int size;
+	User* prev;
 	User* next;
 };
+struct List {
+	User* pHead;
+	User* pTail;
+	int size;
 
-const string userData = "data/users.csv";
 
-void readUsersData(User*& usersData, int& numbers) {
+};
+
+User* createUser(string data) {
+	User* usersData = new User;
+	usersData->id = stoi(data.substr(0, data.find(',')));
+	data.erase(0, data.find(',') + 1);
+	usersData->password = data.substr(0, data.find(','));
+	data.erase(0, data.find(',') + 1);
+	if (data.substr(0, data.find('\n')) == "TRUE") usersData->isAdmin = true;
+	else usersData->isAdmin = false;
+	usersData->next = NULL;
+	usersData->prev = NULL;
+	return usersData;
+}
+void addUser(List& list, User* user) {
+	if (list.pHead == NULL) {
+		list.pHead = list.pTail = user;
+	}
+	else {
+		list.pTail->next = user;
+		user->prev = list.pTail;
+		list.pTail = user;
+	}
+}
+void initList(List& list) {
+	list.pHead = NULL;
+	list.pTail = NULL;
+	list.size = 0;
+}
+void readUsersData(List& listUsers) {
 	ifstream fin(userData);
-	string s, data = "";
+	string  data = "";
 	//Allocate for usersData
-	getline(fin, s);
-	int count = 0;
+	getline(fin, data);
+	initList(listUsers);
 	while (!fin.eof()) {
-		getline(fin, s);
-		if (s != "") {
-			data += s + '\n';
-			count++;
+		getline(fin, data);
+		if (data != "") {
+			addUser(listUsers, createUser(data));
+			listUsers.size++;
 		}
 	}
-	usersData = new User[count];
-	cout << data;
-	//Read data from csv file
-	for (int i = 0; i < count; i++) {
-		usersData[i].id = stoi(data.substr(0, data.find(',')));
-		data.erase(0, data.find(',') + 1);
-		usersData[i].password = data.substr(0, data.find(','));
-		data.erase(0, data.find(',') + 1);
-		if (data.substr(0, data.find('\n')) == "TRUE") usersData[i].isAdmin = true;
-		else usersData[i].isAdmin = false;
-		data.erase(0, data.find('\n') + 1);
-		cout << usersData[i].id << endl;
-		cout << usersData[i].password << endl;
-		cout << usersData[i].isAdmin << endl;
+}
+void printUsersData(List listUsers) {
+	User* temp = listUsers.pHead;
+	while (temp != NULL) {
+		cout << "Username: " << temp->id << endl;
+		cout << "Password: " << temp->password << endl;
+		cout << "Is admin: " << temp->isAdmin << endl;
+		temp = temp->next;
 	}
-
 }
 
 void loginScreen() {
-	User* usersData;
 	int quantity;
-	readUsersData();
+	List listUsers;
+	readUsersData(listUsers);
+	printUsersData(listUsers);
 
 }
